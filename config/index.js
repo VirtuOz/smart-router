4ba1c9bbd2ca6c4f59d09b69ed7158c7c3bde004
Callixte <ccauchois@virtuoz.com>
2012-10-25 09:16:37 -0700

    first stab at the code

---
+++ config/index.js
@@ -0,0 +1,18 @@
+exports.local = 
+{
+  port: 8080,
+  amqp: { url: 'amqp://'},
+  endpoints: [ 
+    { name: 'agent', ids: [ 456 ] },
+    { name: 'ui', ids: [ 456 ] }
+  ]
+  routes: [
+	{ endpoint: 'agent', message: 'talk',
+	  action: function () {}
+    }
+  ]
+}
+
+exports.another = {
+	
+}
